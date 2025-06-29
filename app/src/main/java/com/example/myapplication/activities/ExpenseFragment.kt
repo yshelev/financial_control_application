854138d586aacb39c2aa2bf2dfedbd4777b5fe5d
yobysheva <bobysheva.yv@students.dvfu.ru>
2025-06-30 03:29:07 +1000

    date pickers for diagrams

--- app/src/main/java/com/example/myapplication/activities/ExpenseFragment.kt
+++ app/src/main/java/com/example/myapplication/activities/ExpenseFragment.kt
@@ -3,7 +3,8 @@ package com.example.myapplication
 import com.example.myapplication.database.MainDatabase
 
 class ExpenseFragment : BaseChartFragment() {
-    override val chartTitle = "Расходы"
+    override val chartTitle: String
+        get() = getString(R.string.expense)
     override suspend fun loadData(start: Long, end: Long) =
         MainDatabase.getDatabase(requireContext()).transactionDao()
             .getSumExpenseForChart(start, end)
--- app/src/main/java/com/example/myapplication/activities/IncomeFragment.kt
+++ app/src/main/java/com/example/myapplication/activities/IncomeFragment.kt
@@ -1,9 +1,11 @@
 package com.example.myapplication
 
+import android.util.Log
 import com.example.myapplication.database.MainDatabase
 
 class IncomeFragment : BaseChartFragment() {
-    override val chartTitle = "Доходы"
+    override val chartTitle: String
+        get() = getString(R.string.income)
     override suspend fun loadData(start: Long, end: Long) =
         MainDatabase.getDatabase(requireContext()).transactionDao()
             .getSumIncomeForChart(start, end)
